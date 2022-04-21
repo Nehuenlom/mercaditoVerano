e48f394da4c200bcdee1b0f0313c624292d0ac2a
NehuenLom <93491147+Nehuenlom@users.noreply.github.com>
2022-04-21 13:26:11 -0300

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,12 @@
+let nombreComercio: string;
+nombreComercio = "Mercado El Irlandes Medieval";
+console.log(nombreComercio);
+
+console.log("AZUCAR LEDEZMA, 300 unidades");
+console.log("FERNET BRANCA, 241 unidades");
+console.log("COCA-COLA, 300 unidades");
+console.log("FIDEOS LARGOS, 500 unidades");
+console.log("ARROZ MAROLIO, 427 unidades");
+console.log("DESODORANTE AXE, 220 unidades");
+console.log("LAVANDINA AYUDIN, 400 unidades");
+console.log("LECHE LARGA VIDA, 500 unidades");
